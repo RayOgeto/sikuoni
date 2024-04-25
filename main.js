a359c40a9cb97763b6823c2f277a38b43fb54091
Ogeto <128529806+RayOgeto@users.noreply.github.com>
2024-04-25 10:11:48 +0300

    Trying to fix the video for users

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ const servers = {
 
 let constraints = {
     video:{
-        width:{min:640, ideal:1920, max:1920},
-        height:{min:480, ideal:1080, max:1080},
+        innerWidth:{min:640, ideal:1920, max:1920},
+        height:{min:480, ideal:1080, max:1080}
     },
     audio:true
 }
